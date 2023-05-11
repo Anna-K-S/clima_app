f2eadb6fada7b7e9e8ddab31c6f1b63ec4c6d39f
Anna-K-S <anna.sergeevna1622@gmail.com>
2023-05-11 06:24:51 +0400

    upd

---
+++ lib/text_styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const temperature = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const message = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const button = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const condition = TextStyle(
+  fontSize: 100.0,
+);
